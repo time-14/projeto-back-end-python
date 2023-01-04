33566b244638cbc81e05cae7abcdea7f2fad7922
Mrdiskman <pedrosandes2010@gmail.com>
2023-01-04 10:02:07 -0300

    Feature Register/Login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from .permissions import IsAccountOwner
+from rest_framework import generics
 
-# Create your views here.
+class UserView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_url_kwarg = "user_id"
