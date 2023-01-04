f302020bf1617253ece2c86740a3e056d98f8b38
=Welton Santos <weltonsantosfr@gmail.com>
2023-01-04 11:42:57 -0300

    fix:Vehicle_Info fields

--- vehicle_info/models.py
+++ vehicle_info/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Vehicle_Info(models.Model):
-    engine = models.CharField(50)
-    transmission = models.CharField(50)
+    engine = models.CharField(max_length=50)
+    transmission = models.CharField(max_length=50)
     doors = models.IntegerField()
     seats = models.IntegerField()
-    color = models.CharField(50)
-    vehicle_type = models.CharField(50)
-    gas_type = models.CharField(50)
+    color = models.CharField(max_length=50)
+    vehicle_type = models.CharField(max_length=50)
+    gas_type = models.CharField(max_length=50)
     revisions = models.BooleanField(default=False)
     last_maintance = models.DateField(null=True)
 
